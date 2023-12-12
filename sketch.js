4ce02b2b1a7627fe81b84678c67911eacb7363f9
Ayushi Shah <as17726@nyu.edu>
2023-12-12 17:33:56 -0500

    added character 2 and 3

--- sketch.js
+++ sketch.js
@@ -8,10 +8,14 @@ let image1;
 let counter1 = 0; 
 let x2; 
 let y2; 
-let tellingstory2 = 0; 
+let tellingstory2 = 0;  
+let image2; 
+let counter2 = 0; 
 let x3; 
 let y3; 
 let tellingstory3 = 0;
+let image3; 
+let counter3 = 0; 
 
 //text 
 let MARGIN = 40;
@@ -19,7 +23,7 @@ let currentMaxIndex = 0;
 let nextUpdateMillis = 0;
 
 function preload() { 
-  image1 = loadImage("./Character1.jpg");
+  image1 = loadImage("./Character1.png");
 }
 
 function setup() {
@@ -35,8 +39,8 @@ function setup() {
 }
 
 function draw() {
-  background(183, 54, 26); 
-  if(tellingstory1 ==0) {
+  background(219, 62, 62); 
+  if(tellingstory1 ==0 && tellingstory2 ==0 && tellingstory3 ==0) {
     drawCharacters(); } 
     
   //hover preview
@@ -49,22 +53,24 @@ function draw() {
     ){
       description1(); 
       }
-  // if (
-  //   mouseX > x2 &&
-  //   mouseX < x2 + characterWidth &&
-  //   mouseY > y2 &&
-  //   mouseY < y2 + characterHeight 
-  //   ){
-  //     description2(); 
-  //     }
-  // if (
-  //   mouseX > x3 &&
-  //   mouseX < x3 + characterWidth &&
-  //   mouseY > y3 &&
-  //   mouseY < y3 + characterHeight 
-  //   ){
-  //     description3(); 
-  //     }
+  if (
+    mouseX > x2 &&
+    mouseX < x2 + characterWidth &&
+    mouseY > y2 &&
+    mouseY < y2 + characterHeight && 
+    tellingstory2 == 0
+    ){
+      description2(); 
+      }
+  if (
+    mouseX > x3 &&
+    mouseX < x3 + characterWidth &&
+    mouseY > y3 &&
+    mouseY < y3 + characterHeight && 
+    tellingstory3 == 0
+    ){
+      description3(); 
+      }
 
   if (tellingstory1 == 1) {
     story1();
@@ -75,11 +81,11 @@ function draw() {
 
 function drawCharacters() {
  
-  background(183,54, 26); 
+  background(219, 62, 62); 
 
   character1(x1, y1);
-  //character2(x2, y2);
-  //character3(x3, y3);
+  character2(x2, y2);
+  character3(x3, y3);
 }
 
 function character1(X, Y) {
@@ -109,9 +115,12 @@ function character1(X, Y) {
    ellipse(30, 0, 30, 30); 
 
   // Eyes
-  fill(0);
+  fill(255);
   ellipse(20, 40, 10, 10);
   ellipse(40, 40, 10, 10);
+  fill(0);
+  ellipse(20, 40, 5, 5);
+  ellipse(40, 40, 5, 5);
 
   //smile 
   fill(255, 0, 0); 
@@ -134,66 +143,65 @@ function character1(X, Y) {
   pop(); 
 } 
 
-// function character2(X,Y) {
-//   //x2, y2
-//   push();
-//   translate(X,Y);
+function character2(X,Y) {
+  //x2, y2
+  push();
+  translate(X,Y);
 
-//   stroke(0);
-//   strokeWeight(1);
+  stroke(0);
+  strokeWeight(1);
 
-//   //arms
-//   fill("SaddleBrown");
-//   rect(0, 55, 20, 50);
-//   rect(40, 55, 20, 50);
+  //arms
+  fill("SaddleBrown");
+  rect(0, 55, 20, 50);
+  rect(40, 55, 20, 50);
 
-//    // Body
-//    fill("DarkSeaGreen");
-//    rect(10, 55, 40, 50);
+   // Body
+   fill("DarkSeaGreen");
+   rect(10, 55, 40, 50);
 
-//    // Face
-//    fill("SaddleBrown");
-//    ellipse(30, 35, 50, 50);
+   // Face
+   fill("SaddleBrown");
+   ellipse(30, 35, 50, 50);
 
-//   // Hair
-//   fill(0);
-//   arc(30, 30, 50, 50, PI, 0, CHORD);
-//   ellipse(9, 75, 15, 50); 
-//   ellipse(51, 75, 15, 50); 
+  // Hair
+  fill(0);
+  arc(30, 30, 50, 50, PI, 0, CHORD);
+  ellipse(9, 75, 15, 50); 
+  ellipse(51, 75, 15, 50); 
 
-//  // Eyes
-//  fill(0);
-//  ellipse(20, 40, 10, 10);
-//  ellipse(40, 40, 10, 10);
+ // Eyes
+ fill(0);
+ ellipse(20, 40, 10, 10);
+ ellipse(40, 40, 10, 10);
 
-//  //smile 
-//  fill(255, 0, 0); 
-//  arc(30, 50, 10, 10, 0, PI, CHORD);
+ //smile 
+ fill(255, 0, 0); 
+ arc(30, 50, 10, 10, 0, PI, CHORD);
 
-//  // Skirt
-//  fill("AntiqueWhite");
-//  beginShape(); 
-//  vertex(10, 105); 
-//  vertex(50, 105); 
-//  vertex(60, 150); 
-//  vertex(0, 150);
-//  endShape(); 
+ // Skirt
+ fill("AntiqueWhite");
+ beginShape(); 
+ vertex(10, 105); 
+ vertex(50, 105); 
+ vertex(60, 150); 
+ vertex(0, 150);
+ endShape(); 
 
-// // Legs
-// fill(0);
-// rect(15, 150, 20, 5);
-// rect(25, 150, 20, 5);
+// Legs
+fill(0);
+rect(15, 150, 20, 5);
+rect(25, 150, 20, 5);
 
-//  pop(); 
+ pop(); 
 
 
-// }
-// function character3() {
+}
+
+function character3() {
   //x3, y3
-// }
-// function character4() {
-  //x4, y4
-// }
+  
+}
 
 function speechbubble(X, Y) {
   let bubbleWidth = 150; 
@@ -213,14 +221,31 @@ function speechbubble(X, Y) {
 
  function description1() {
   speechbubble(x1, 120); 
+  textSize(20);
+  fill(0);
+  stroke(0);
+  strokeWeight(1);
+
+  text('This is a test', x1+10, 120+30, 120, 100 ); 
  }
 
-// function description2() {
-//   speechbubble(x2, 120); 
-//  }
+function description2() {
+  speechbubble(x2, 120); 
+  textSize(20);
+  fill(0);
+  stroke(0);
+  strokeWeight(1);
+  text('This is a test', x2+10, 120+30, 120, 100 ); 
+ }
+
 
 function desciption3() {
-  speechbubble(70, 120); 
+  speechbubble(x3, 120); 
+  textSize(20);
+  fill(0);
+  stroke(0);
+  strokeWeight(1);
+  text('This is a test', x3+10, 120+30, 120, 100 );
 }
 
 // function description4() {
@@ -234,15 +259,15 @@ function desciption3() {
   textAlign(LEFT, TOP);
   textSize(20);
 
-  background(183,54, 26);
+  background(219, 62, 62);
 
   if(keyIsPressed) {
     if(key == 'a') {
-      counter1++; 
-      image(image1, 0, 0);  
+      counter1++;  
+      image1.resize(500, 450); 
+      image(image1, 0, 0);
       } 
     }
-
   // if(counter1 == 1 ) {
   //   if(keyIsPressed) {
   //     if (key == 'a') {
@@ -259,9 +284,6 @@ function desciption3() {
   //     }
   //   }
   // }
-
-
-
  }
 // function story2() {
   //button 1 - click through story, audio, visuals 
@@ -285,31 +307,29 @@ function mouseClicked() {
         //if (!mSerial.opened())
         tellingstory1 = 1; 
        }
-//    else if (
-//     mouseX > x2 &&
-//     mouseX < x2 + characterWidth &&
-//     mouseY > y2 &&
-//     mouseY < y2 + characterHeight
-//     )  
-//      { story2();
-//       }
-//    else if (
-//     mouseX > x3 &&
-//     mouseX < x3 + characterWidth &&
-//     mouseY > y3 &&
-//     mouseY < y3 + characterHeight
-//     )  
-//      { story3();
-//       }
-//    else if (
-//     mouseX > x4 &&
-//     mouseX < x4 + characterWidth &&
-//     mouseY > y4 &&
-//     mouseY < y4 + characterHeight
-//     )  
-//      { story4();
-//       }
+    else if (
+      mouseX > x2 &&
+      mouseX < x2 + characterWidth &&
+      mouseY > y2 &&
+      mouseY < y2 + characterHeight
+     )  
+      { //connecttoserial
+        //if function - open serial only if it already is not open 
+        //if (!mSerial.opened())
+        tellingstory2 = 1; 
+       }
+      else if (
+        mouseX > x3 &&
+        mouseX < x3 + characterWidth &&
+        mouseY > y3 &&
+        mouseY < y3 + characterHeight
+     )  
+      { //connecttoserial
+        //if function - open serial only if it already is not open 
+        //if (!mSerial.opened())
+        tellingstory3 = 1; 
+       }
+
 
  }
-
   
