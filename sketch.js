5a9cf86a7355734cb71bec976c5f72b4bdca8439
Ayushi Shah <as17726@nyu.edu>
2023-12-12 13:03:05 -0500

    updated story1

--- sketch.js
+++ sketch.js
@@ -3,15 +3,24 @@ let characterWidth = 80;
 let characterHeight = 300; 
 let x1; 
 let y1;
+let tellingstory1 = 0; 
+let image1; 
+let counter1 = 0; 
 let x2; 
 let y2; 
+let tellingstory2 = 0; 
 let x3; 
 let y3; 
+let tellingstory3 = 0;
 
 //text 
 let MARGIN = 40;
 let currentMaxIndex = 0;
 let nextUpdateMillis = 0;
+
+function preload() { 
+  image1 = loadImage("./Character1.jpg");
+}
 
 function setup() {
     createCanvas(1200, windowHeight);
@@ -21,46 +30,45 @@ function setup() {
     y2 = windowHeight/2; 
     x3 = 1100; 
     y3 = windowHeight/2; 
-    
+
+    pixelDensity(1);
 }
 
 function draw() {
   background(183, 54, 26); 
-  drawCharacters();
+  if(tellingstory1 ==0) {
+    drawCharacters(); } 
     
   //hover preview
   if (
     mouseX > x1 &&
     mouseX < x1 + characterWidth &&
     mouseY > y1 &&
-    mouseY < y1 + characterHeight 
+    mouseY < y1 + characterHeight && 
+    tellingstory1 == 0
     ){
       description1(); 
       }
-  if (
-    mouseX > x2 &&
-    mouseX < x2 + characterWidth &&
-    mouseY > y2 &&
-    mouseY < y2 + characterHeight 
-    ){
-      description2(); 
-      }
-  if (
-    mouseX > x3 &&
-    mouseX < x3 + characterWidth &&
-    mouseY > y3 &&
-    mouseY < y3 + characterHeight 
-    ){
-      //description3(); 
-      }
-     // if (
-    //   mouseX > x4 &&
-    //   mouseX < x4 + characterWidth &&
-    //   mouseY > y4 &&
-    //   mouseY < y4 + characterHeight 
-    //   ){
-    //     description4(); 
-    //    }
+  // if (
+  //   mouseX > x2 &&
+  //   mouseX < x2 + characterWidth &&
+  //   mouseY > y2 &&
+  //   mouseY < y2 + characterHeight 
+  //   ){
+  //     description2(); 
+  //     }
+  // if (
+  //   mouseX > x3 &&
+  //   mouseX < x3 + characterWidth &&
+  //   mouseY > y3 &&
+  //   mouseY < y3 + characterHeight 
+  //   ){
+  //     description3(); 
+  //     }
+
+  if (tellingstory1 == 1) {
+    story1();
+  } 
   
   //button 2 - return to homepage 
 }
@@ -70,7 +78,7 @@ function drawCharacters() {
   background(183,54, 26); 
 
   character1(x1, y1);
-  character2(x2, y2);
+  //character2(x2, y2);
   //character3(x3, y3);
 }
 
@@ -126,60 +134,60 @@ function character1(X, Y) {
   pop(); 
 } 
 
-function character2(X,Y) {
-  //x2, y2
-  push();
-  translate(X,Y);
+// function character2(X,Y) {
+//   //x2, y2
+//   push();
+//   translate(X,Y);
 
-  stroke(0);
-  strokeWeight(1);
+//   stroke(0);
+//   strokeWeight(1);
 
-  //arms
-  fill("SaddleBrown");
-  rect(0, 55, 20, 50);
-  rect(40, 55, 20, 50);
+//   //arms
+//   fill("SaddleBrown");
+//   rect(0, 55, 20, 50);
+//   rect(40, 55, 20, 50);
 
-   // Body
-   fill("DarkSeaGreen");
-   rect(10, 55, 40, 50);
+//    // Body
+//    fill("DarkSeaGreen");
+//    rect(10, 55, 40, 50);
 
-   // Face
-   fill("SaddleBrown");
-   ellipse(30, 35, 50, 50);
+//    // Face
+//    fill("SaddleBrown");
+//    ellipse(30, 35, 50, 50);
 
-  // Hair
-  fill(0);
-  arc(30, 30, 50, 50, PI, 0, CHORD);
-  ellipse(9, 75, 15, 50); 
-  ellipse(51, 75, 15, 50); 
+//   // Hair
+//   fill(0);
+//   arc(30, 30, 50, 50, PI, 0, CHORD);
+//   ellipse(9, 75, 15, 50); 
+//   ellipse(51, 75, 15, 50); 
 
- // Eyes
- fill(0);
- ellipse(20, 40, 10, 10);
- ellipse(40, 40, 10, 10);
+//  // Eyes
+//  fill(0);
+//  ellipse(20, 40, 10, 10);
+//  ellipse(40, 40, 10, 10);
 
- //smile 
- fill(255, 0, 0); 
- arc(30, 50, 10, 10, 0, PI, CHORD);
+//  //smile 
+//  fill(255, 0, 0); 
+//  arc(30, 50, 10, 10, 0, PI, CHORD);
 
- // Skirt
- fill("AntiqueWhite");
- beginShape(); 
- vertex(10, 105); 
- vertex(50, 105); 
- vertex(60, 150); 
- vertex(0, 150);
- endShape(); 
+//  // Skirt
+//  fill("AntiqueWhite");
+//  beginShape(); 
+//  vertex(10, 105); 
+//  vertex(50, 105); 
+//  vertex(60, 150); 
+//  vertex(0, 150);
+//  endShape(); 
 
-// Legs
-fill(0);
-rect(15, 150, 20, 5);
-rect(25, 150, 20, 5);
+// // Legs
+// fill(0);
+// rect(15, 150, 20, 5);
+// rect(25, 150, 20, 5);
 
- pop(); 
+//  pop(); 
 
 
-}
+// }
 // function character3() {
   //x3, y3
 // }
@@ -207,9 +215,9 @@ function speechbubble(X, Y) {
   speechbubble(x1, 120); 
  }
 
-function description2() {
-  speechbubble(x2, 120); 
- }
+// function description2() {
+//   speechbubble(x2, 120); 
+//  }
 
 function desciption3() {
   speechbubble(70, 120); 
@@ -222,19 +230,38 @@ function desciption3() {
  function story1() {
   //button 1 - click through story, audio, visuals 
   let phrase = "This is a test";  
+  let end = "The End. Click d to go back to main page."; 
   textAlign(LEFT, TOP);
   textSize(20);
 
   background(183,54, 26);
-  
-  if (millis() > nextUpdateMillis) {
-    currentMaxIndex = min(currentMaxIndex + 1, phrase.length);
 
-    nextUpdateMillis = millis() + random(30, 160);
-  }
+  if(keyIsPressed) {
+    if(key == 'a') {
+      image(image1, 0, 0); 
+      counter1++; 
+      }
+    }
 
-  let phraseToDraw = phrase.slice(0, currentMaxIndex);
-  text(phraseToDraw, MARGIN, MARGIN, width - 2 * MARGIN, height);
+  // if(counter1 == 1 ) {
+  //   if(keyIsPressed) {
+  //     if (key == 'a') {
+  //       if (millis() > nextUpdateMillis) {
+  //         currentMaxIndex = min(currentMaxIndex + 1, phrase.length);
+      
+  //         nextUpdateMillis = millis() + random(30, 160);
+  //       }
+      
+  //       let phraseToDraw = phrase.slice(0, currentMaxIndex);
+  //       text(phraseToDraw, MARGIN, MARGIN, width - 2 * MARGIN, height);
+
+  //       counter1++; 
+  //     }
+  //   }
+  // }
+
+
+
  }
 // function story2() {
   //button 1 - click through story, audio, visuals 
@@ -256,8 +283,7 @@ function mouseClicked() {
       { //connecttoserial
         //if function - open serial only if it already is not open 
         //if (!mSerial.opened())
-        print(mouseX); 
-        story1();
+        tellingstory1 = 1; 
        }
 //    else if (
 //     mouseX > x2 &&
